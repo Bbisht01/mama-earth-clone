d3c446e86388610555fc969e661e66951b5765eb
Nikita Patel <gniki0830@gmail.com>
2021-12-15 15:03:00 +0530

    navbar code

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 function navbar(){
-    return `
+    return `<nav>
+    
     <div id = "nav1">
     <a href = ""><p>Asia's first brand with MADE SAFE certified products</p></a>
     <a href = ""><p id = "navt">Free Shippping above 399 | COD Available | Ertra 5% OFF
@@ -146,6 +147,48 @@ function navbar(){
     <p class="hometxt">PLANT GOODNESS</p>
     <p class="hometxt">STORE LOCATOR</p>
 </div>
+
+<div class="carousel">
+       
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://images.ctfassets.net/66mrrren2unf/4I2Ji1TxrGoZUZi1LpO9lb/945ef7e2040f21de43c0f327ead451b5/desktop-body-lotion.jpg?q=40" alt="Los Angeles" style="width:100%;">
+    </div>
+
+    <div class="item">
+      <img src="https://images.ctfassets.net/66mrrren2unf/rBK093QawZY4YOdbxSjHJ/7bac4d18e5638b28b13a27315564b14d/Desktop.jpg?q=40" alt="Chicago" style="width:100%;">
+    </div>
+  
+    <div class="item">
+      <img src="https://images.ctfassets.net/66mrrren2unf/62fMX7z3RN55j8FpWnIxhg/1a194a111a058b377248805cdea8449d/Desktop_Without_CTA.jpg?q=40" alt="New york" style="width:100%;">
+    </div>
+
+    <div class="item">
+        <img src="https://images.ctfassets.net/66mrrren2unf/5OhEUHOLxFpTkjZ2Zk2Y4S/f1ccaaf5d228df39ccd858841b23d997/Desktop.jpg?q=40" alt="New york" style="width:100%;">
+      </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+</nav>
  `
 }
 
