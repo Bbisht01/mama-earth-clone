b951fa70bb823e9bd19b8b6fe80dcf86035eeed0
Nikita Patel <gniki0830@gmail.com>
2021-12-17 16:43:20 +0530

    final code 2

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ function navbar() {
     
     <div id = "nav1">
     <a href = ""><p>Asia's first brand with MADE SAFE certified products</p></a>
-    <a href = ""><p id = "navt">Free Shippping above 399 | COD Available | Ertra 5% OFF
+    <a href = ""><p id = "navt">Free Shippping above 399 | COD Available | Extra 5% OFF
         on prepaid orders | Shop Now
     </p></a>
     <i class="far fa-user"></i>
@@ -14,14 +14,14 @@ function navbar() {
         <input id = "srch" type = "text" placeholder = "Search for products.."><button id = "btn"><i class="fas fa-search"></i>Search</button>
     </div>
         
-    <i class="fas fa-shopping-cart fa-lg"></i> Cart
+    <a href = "cart.html"><i class="fas fa-shopping-cart fa-lg"></i></a> Cart
     <i class="fas fa-user-alt fa-lg"></i> Login
 </div>
 <div id = "nav3">
     <p id="homet">HOME</p>
     
     <div class="dropdown"> <p>BABY</p> <div class="dropdown-content">
-        <ul>
+        <ul class = "inline">
             <li class="bold">Explore</li>
             <li>New Launchers</li>
             <li>Best Sellers</li>
@@ -29,7 +29,7 @@ function navbar() {
             <li>Gift Packs</li>
         </ul>
         
-        <ul>
+        <ul class = "inline">
             <li class="bold">Baby</li>
             <li>Baby Shampoo</li>
             <li>Baby Oralcare</li>
@@ -39,14 +39,14 @@ function navbar() {
             <li>Baby Diaper</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="blue">See All</li>
         </ul>
     </div> </div>
 
 
     <div  id="beauty" class="dropdown"> <p>BEAUTY</p> <div class="dropdown-content">
-        <ul>
+    <ul class = "inline">
             <li class="bold">Explore</li>
             <li>New Launchers</li>
             <li>Best Sellers</li>
@@ -54,21 +54,21 @@ function navbar() {
             <li>Gift Packs</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="bold">Beauty</li>
             <li>Face</li>
             <li>Hair</li>
             <li>Body</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="blue">See All</li>
         </ul>
     </div> </div>
 
 
     <div  id="hair" class="dropdown"> <p>HAIR</p> <div class="dropdown-content">
-        <ul>
+    <ul class = "inline">
             <li class="bold">Explore</li>
             <li>New Launchers</li>
             <li>Best Sellers</li>
@@ -76,7 +76,7 @@ function navbar() {
             <li>Gift Packs</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="bold">Hair</li>
             <a id="shampoo" href="Products.html"><li>Shampoo</li></a>
             <li>conditioner</li>
@@ -85,14 +85,14 @@ function navbar() {
             <li>Hair Serum</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="blue">See All</li>
         </ul>
     </div> </div>
 
 
     <div  id="face" class="dropdown"> <p>FACE</p> <div class="dropdown-content">
-        <ul>
+    <ul class = "inline">
             <li class="bold">Explore</li>
             <li>New Launchers</li>
             <li>Best Sellers</li>
@@ -100,7 +100,7 @@ function navbar() {
             <li>Gift Packs</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="bold">Face</li>
             <li>Face Wash</li>
             <li>Face Mask</li>
@@ -112,14 +112,14 @@ function navbar() {
             <li>Face Sheet Mask</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="blue">See All</li>
         </ul>
     </div> </div>
 
 
     <div  id="body" class="dropdown"> <p>BODY</p> <div class="dropdown-content">
-        <ul>
+    <ul class = "inline">
             <li class="bold">Explore</li>
             <li>New Launchers</li>
             <li>Best Sellers</li>
@@ -127,7 +127,7 @@ function navbar() {
             <li>Gift Packs</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="bold">Bodyy</li>
             <li>Body Butter</li>
             <li>Body Wash</li>
@@ -136,7 +136,7 @@ function navbar() {
             <li>Hand Cream</li>
         </ul>
 
-        <ul>
+        <ul class = "inline">
             <li class="blue">See All</li>
         </ul>
     </div> </div>
